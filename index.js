55b9fe94f2ba477ca9077decfc2572093ae80325
Jerry Sievert <code@legitimatesounding.com>
2013-08-18 09:31:41 -0700

    remove deferreds for terraformer geostore change

--- index.js
+++ index.js
@@ -7,63 +7,99 @@ function LevelStore (options) {
   this.db = levelup(this.name);
 }
 
-LevelStore.prototype.add = function (geojson, dfd) {
+LevelStore.prototype.add = function (geojson, callback) {
   if (geojson.type === "FeatureCollection"){
-    for (var i = 0; i < geojson.features.length; i++) {
+    var done = false, count = 0;
+    for (var i = 0; i < geojson.features.length && !done; i++) {
       this.db.put(geojson.features[i].id, JSON.stringify(geojson.features[i]), function (err) {
+        count++;
+
         if (err) {
-          dfd.reject(err);
+          done = true;
+          if (callback) {
+            callback(err);
+          }
+        } else {
+          if (count === geojson.features.length) {
+            if (callback) {
+              callback(null);
+            }            
+          }
         }
       });
+    }
+
+    if (done) {
+      return;
     }
   } else {
     this.db.put(geojson.id, JSON.stringify(geojson), function (err) {
       if (err) {
-        dfd.reject(err);
+        if (callback) {
+          callback(err);
+          return;
+        }
+      } else {
+        if (callback) {
+          callback(null);
+        }
       }
     });
   }
-
-  dfd.resolve(geojson);
-  return dfd;
 };
 
-LevelStore.prototype.get = function (id, dfd) {
+LevelStore.prototype.get = function (id, callback) {
   this.db.get(id, function (err, data) {
     if (err) {
-      dfd.reject(err);
+      if (callback) {
+        callback(err, data);
+      }
     } else {
-      dfd.resolve(JSON.parse(data));
+      if (callback) {
+        callback(null, data);
+      }
     }
   });
-
-  return dfd;
 };
 
-LevelStore.prototype.update = function (geojson, dfd) {
-  return this.add(JSON.stringify(geojson), dfd);
+LevelStore.prototype.update = function (geojson, callback) {
+  this.add(geojson, callback);
 };
 
-LevelStore.prototype.remove = function (id, dfd) {
-  if (geojson.type === "FeatureCollection"){
-    for (var i = 0; i < geojson.features.length; i++) {
+LevelStore.prototype.remove = function (id, callback) {
+  if (geojson.type === "FeatureCollection") {
+    var done = false, count = 0;
+    for (var i = 0; i < geojson.features.length && !done; i++) {
       this.db.del(geojson.features[i].id, function (err) {
+        count++;
+
         if (err) {
-          dfd.reject(err);
+          done = true;
+          if (callback) {
+            callback(err);
+          }
+        } else {
+          if (count === geojson.features.length) {
+            if (callback) {
+              callback(null);
+            }            
+          }
         }
       });
     }
   } else {
     this.db.del(geojson.id, function (err) {
       if (err) {
-        dfd.reject(err);
+        if (callback) {
+          callback(err);
+        }
+      } else {
+        if (callback) {
+          callback(null, geojson.id);
+        }
       }
     });
   }
-
-  dfd.resolve(geojson);
-  return dfd;
-
 };
 
 exports.LevelStore = LevelStore;
